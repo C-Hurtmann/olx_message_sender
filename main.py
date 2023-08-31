b17f71cad497bc242be4b9f12e01217d03dda182
C-Hurtmann <constantine2903@gmail.com>
2023-08-31 10:47:56 +0300

    add login cookies

--- main.py
+++ main.py
@@ -2,29 +2,30 @@ from time import sleep
 import json
 
 from selenium import webdriver
+from selenium.common.exceptions import InvalidCookieDomainException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+options = webdriver.ChromeOptions()
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+options.add_argument(f"User-Agent={user_agent}")
+
 with open('olx_cookies.json', 'r') as f:
     cookies = json.load(f)
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.olx.ua/account/")
 
 for cookie in cookies:
     if cookie['domain'] == 'ua.login.olx.com':
-        print(cookie['domain'])
-        cookie['sameSite'] = "Lax"
+        cookie['sameSite'] = 'None'
         driver.add_cookie(cookie)
 
-driver.get('https://ua.login.olx.com/')
-
-for cookie in cookies:
-    if cookie['domain'] != 'ua.login.olx.com':
-        print(cookie['domain'])
-        cookie['sameSite'] = "Lax"
-        driver.add_cookie(cookie)
+driver.refresh()
 
 wait = WebDriverWait(driver, 10)
 
@@ -32,7 +33,6 @@ wait = WebDriverWait(driver, 10)
 #send_message_button.click()
 
 sleep(100)
-
 html_content = driver.page_source
 
 with open('index.html', 'w') as f:
